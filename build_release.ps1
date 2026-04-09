fcc26a5ee6483ba51939b91f6e8c4c8050a8a216
Alon <artalon.contact@gmail.com>
2026-04-09 02:26:18 +0100

    Fix Thunderstore Author parsing mechanism and Zip structure

--- build_release.ps1
+++ build_release.ps1
@@ -41,7 +41,7 @@ Copy-Item "README.md" -Destination $TempDir
 $ManifestContent = Get-Content "Thunderstore\manifest.json" | ConvertFrom-Json
 $Version = $ManifestContent.version_number
 
-$ZipName = "releases\SimpleBackup-v$Version.zip"
+$ZipName = "releases\$($ManifestContent.name)-v$Version.zip"
 
 if (Test-Path $ZipName) {
     Remove-Item -Force $ZipName
